35e5b4e4d65d9963bc90c9e771ae3770871b1ac9
Tomi Vesanen <vivesanto@student.hamk.fi>
2016-05-10 11:49:07 +0300

    Ehdokaskyselyn luonti aloitettu

---
+++ src/java/vaalikone/Ehdokas.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vaalikone;
+
+/**
+ *
+ * @author vivesanto
+ */
+public class Ehdokas extends Kayttaja{
+    private int ehdokasId;
+}
--- src/java/vaalikone/EhdokasKysely.java
+++ src/java/vaalikone/EhdokasKysely.java
@@ -6,11 +6,18 @@
 package vaalikone;
 
 import java.io.IOException;
+import static java.lang.Integer.parseInt;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import persist.Kysymykset;
+import persist.Vastaukset;
 
 /**
  *
@@ -21,14 +28,68 @@ public class EhdokasKysely implements Moduuli {
     private Kayttaja usr;
     private EntityManager em;
     private Logger logger;
+    private HttpSession session;
 
     @Override
     public void ajaModuuli(HttpServletRequest request, HttpServletResponse response, Vaalikone vaalikone) throws ServletException, IOException {
 
+        int kysymys_id;
+
+        //hae parametrinä tuotu edellisen kysymyksen nro
+        String strKysymys_id = request.getParameter("q");
+
+        //hae parametrina tuotu edellisen kysymyksen vastaus
+        String strVastaus = request.getParameter("vastaus");
+        
         // Haetaan Vaalikoneeseen tallennetut Käyttäjä, EntityManager ja Logger
         usr = vaalikone.getUsr();
         em = vaalikone.getEm();
         logger = Logger.getLogger(Loki.class.getName());
+        session = vaalikone.getSession();
+        
+        logger.log(Level.FINE, "Sessio ID: {0}", session.getId());
+        
+        // Jos kysymyksen numero (kysId) on asetettu, haetaan tuo kysymys
+        // muuten haetaan kysnro 1
+        if (strKysymys_id == null) {
+            kysymys_id = 1;
+        } else {
+            kysymys_id = parseInt(strKysymys_id);
+            //jos vastaus on asetettu, tallenna se session käyttäjä-olioon
+            if (strVastaus != null) {
+                usr.addVastaus(kysymys_id, parseInt(strVastaus));
+            }
+
+            //määritä seuraavaksi haettava kysymys
+            kysymys_id++;
+        }
+        
+        //jos kysymyksiä on vielä jäljellä, hae seuraava
+        if (kysymys_id < 20) {
+            try {
+                //Hae haluttu kysymys tietokannasta
+                Query q = em.createQuery(
+                        "SELECT k FROM Kysymykset k WHERE k.kysymysId=?1");
+                q.setParameter(1, kysymys_id);
+                //Lue haluttu kysymys listaan
+                List<Kysymykset> kysymysList = q.getResultList();
+                request.setAttribute("kysymykset", kysymysList);
+                request.getRequestDispatcher("/vastaus.jsp")
+                        .forward(request, response);
+
+            } finally {
+                // Sulje tietokantayhteys
+                if (em.getTransaction().isActive()) {
+                    em.getTransaction().rollback();
+                }
+                em.close();
+            }
+
+            //jos kysymykset loppuvat, lasketaan tulos!
+        } else {
+            //siirrytään hakemaan paras ehdokas
+            vaalikone.setStrFunc("haeEhdokas");
+        }
     }
 
 }
